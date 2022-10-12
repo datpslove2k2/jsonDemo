200f27c3d9a89f31272d6c115ee5472d42f52b12
datpslove2k2 <datpslove98@gmail.com>
2022-10-12 15:56:01 +0700

    upp

---
+++ CallApi/app/src/main/java/com/example/callapi/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.callapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = (TextView) findViewById(R.id.text_view_result);
+        Button buttonParse = (Button) findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
+    }
+    private void jsonParse() {
+        String url = "https://api.jsonserve.com/4TtJo8";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("employee");
+                            for (int i = 0; i<jsonArray.length(); i++) {
+                                JSONArray employ = jsonArray.getJSONArray(i);
+
+                                String firtName = employ.getString("firstname");
+                                int age = employ.getInt(age);
+                                String mail = employ.getString("mail");
+                                mTextViewResult.append(firtName + "," + String.valueOf(age) + "," + mail + "\n\n");
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+}
